1066688d98b75cde5802324ef72571ee9afc3554
v.kopychko <v.kopychko@pay-lab.ru>
2026-02-12 16:17:49 +0300

    Добавление каркаса для RecyclerView

--- app/src/main/java/com/example/seedlings/CalendarFragment.kt
+++ app/src/main/java/com/example/seedlings/CalendarFragment.kt
@@ -8,16 +8,27 @@ import androidx.fragment.app.Fragment
 
 class CalendarFragment : Fragment(R.layout.fragment_calendar)
 {
+    private lateinit var listFragment: ListFragment
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val calendar: CalendarView = view.findViewById(R.id.calendar)
         val dateView: TextView = view.findViewById(R.id.date_view)
 
-        calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+//        calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+//            val date = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
+//            dateView.text = date
+//        }
+//
+        calendar.setOnDateChangeListener {_, year, month, dayOfMonth ->
             val date = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
-            dateView.text = date
+            listFragment = ListFragment()
+
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.top_container, listFragment, "list")
+                .addToBackStack(null)
+                .commit()
         }
     }
 }
-
---
+++ app/src/main/java/com/example/seedlings/ListFragment.kt
@@ -0,0 +1,11 @@
+package com.example.seedlings
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import com.example.seedlings.R
+
+class ListFragment : Fragment(R.layout.fragment_list)
