a795d0b23f4b2a09b6f0ea69040299be4f3479ec
v.kopychko <v.kopychko@pay-lab.ru>
2026-02-19 02:41:23 +0300

    Актуализировал библиотеки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,23 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlinSerialization)
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt)
+}
+
+kapt {
+    correctErrorTypes = true
 }
 
 android {
     namespace = "com.example.seedlings"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.seedlings"
-        minSdk = 31
-        targetSdk = 36
+        minSdk = 26
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -20,22 +27,21 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
-    buildFeatures{
-        dataBinding = true
+    buildFeatures {
         viewBinding = true
+    }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
     }
 }
 
@@ -46,21 +52,21 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    implementation("androidx.fragment:fragment:1.8.5")
-    implementation("androidx.databinding:databinding-runtime:8.10.0")
-    implementation("it.xabaras.android:recyclerview-swipedecorator:1.4")
-    implementation(libs.net.okhttp)
-    implementation(libs.net.okhttp.logging)
-    implementation(libs.net.retrofit)
-    implementation(libs.net.retrofit.kotlin)
+
     implementation(libs.kotlin.dateTime)
     implementation(libs.kotlin.serialization.core)
     implementation(libs.kotlin.serialization.json)
     implementation(libs.kotlin.coroutines.core)
     implementation(libs.kotlin.coroutines.android)
+
+    implementation(libs.net.okhttp)
+    implementation(libs.net.okhttp.logging)
+    implementation(libs.net.retrofit)
+    implementation(libs.net.retrofit.kotlin)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
     implementation(libs.lifecycle.viewmodel)
 
     testImplementation(libs.junit.junit)
@@ -68,6 +74,7 @@ dependencies {
     testImplementation(libs.kotlin.mockito)
     testImplementation(libs.kotlin.coroutines.test)
     testImplementation(libs.androidx.testing)
-
-    implementation(libs.hilt.android)
+    implementation("androidx.fragment:fragment:1.8.5")
+    implementation("androidx.databinding:databinding-runtime:8.10.0")
+    implementation("it.xabaras.android:recyclerview-swipedecorator:1.4")
 }
